73fd99a8ebdcb76a98a9fcf4f60ebffdff296d41
Daniel-Lidman <dlidman@gmail.com>
2025-07-06 17:38:08 -0500

    set up video embed for twitter and youtube and vimeo

--- app/_components/DLVideoInspirationBoardComponent.js
+++ app/_components/DLVideoInspirationBoardComponent.js
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Tweet } from 'react-tweet';
 
 function DLVideoInspirationBoardComponent({ allLikes }) {
   let likes = allLikes;
@@ -59,7 +60,7 @@ function VideoDetails({ selectedVideo }) {
       <div className="mb-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
         <div className="m-5">
           <div className="w-full rounded-md">
-            <VimeoEmbedCode video={video} />
+            <VideoEmbedCode video={video} />
           </div>
         </div>
         <VideoInfo video={video} />
@@ -89,12 +90,22 @@ function VideoInfo({ video }) {
   );
 }
 
-function VimeoEmbedCode({ video }) {
-  if (!video.url) {
+function VideoEmbedCode({ video }) {
+  // NO VIDEO URL
+
+  // VIMEO
+  if (video.url.includes('vimeo.com')) {
+    // Extract Vimeo video ID
+    const match = video.url.match(/vimeo\.com\/(?:.*\/)?(\d+)/);
+    if (!match) {
+      throw new Error('Invalid Vimeo URL');
+    }
+    const vimeoId = match[1];
+
     return (
       <div className="relative h-0 overflow-hidden rounded-xl pb-[56.25%] shadow-md">
         <iframe
-          src={`https://player.vimeo.com/video/724972964?h=59d1872ce3&autoplay=0`}
+          src={`https://player.vimeo.com/video/${vimeoId}?h=59d1872ce3&autoplay=0`}
           frameBorder="0"
           allow="fullscreen; picture-in-picture"
           allowFullScreen
@@ -104,22 +115,54 @@ function VimeoEmbedCode({ video }) {
     );
   }
 
-  // Extract Vimeo video ID
-  const match = video.url.match(/vimeo\.com\/(?:.*\/)?(\d+)/);
-  if (!match) {
-    throw new Error('Invalid Vimeo URL');
-  }
-  const vimeoId = match[1];
+  // YOUTUBE
+  if (video.url.includes('youtube.com') || video.url.includes('youtu.be')) {
+    const id = video.url.split('?v=')[1];
 
-  return (
-    <div className="relative h-0 overflow-hidden rounded-xl pb-[56.25%] shadow-md">
-      <iframe
-        src={`https://player.vimeo.com/video/${vimeoId}?h=59d1872ce3&autoplay=0`}
-        frameBorder="0"
-        allow="fullscreen; picture-in-picture"
-        allowFullScreen
-        className="absolute left-0 top-0 h-full w-full"
-      ></iframe>
-    </div>
-  );
+    return (
+      <div className="relative h-0 overflow-hidden rounded-xl pb-[56.25%] shadow-md">
+        <iframe
+          // width="full"
+          // height="full"
+          src={`https://www.youtube.com/embed/${id}`}
+          title="YouTube video player"
+          frameborder="0"
+          allowfullscreen
+          className="aspect-video w-full"
+        ></iframe>
+      </div>
+    );
+  }
+
+  // TWITTER / X
+  if (video.url.includes('twitter.com' || video.url.includes('x.com'))) {
+    const id = video.url.split('status/')[1];
+
+    console.log(id);
+
+    return <Tweet id={id} />;
+  }
+
+  if (video.url.includes('instagram.com')) {
+    // INSTAGRAM EMBED CODE HERE
+    return <></>;
+  } else {
+    return (
+      <div className="relative h-0 overflow-hidden rounded-xl pb-[56.25%] shadow-md">
+        <img src={video.thumbnail} alt="No embeddable video" width="full" />
+        {/* <iframe
+          src={`https://player.vimeo.com/video/724972964?h=59d1872ce3&autoplay=0`}
+          frameBorder="0"
+          allow="fullscreen; picture-in-picture"
+          allowFullScreen
+          className="absolute left-0 top-0 h-full w-full"
+        ></iframe> */}
+      </div>
+    );
+  }
 }
+
+/*
+Error catching:
+
+*/
