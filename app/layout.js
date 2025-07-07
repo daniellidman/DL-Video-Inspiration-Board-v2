be979acd0706ca4381e43133a1ec5890baf283f8
Daniel-Lidman <dlidman@gmail.com>
2025-07-07 13:08:56 -0500

    added edit and new icons

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import '@/app/_styles/globals.css';
 import Logo from './_components/logo';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'DL Video Inspiration Board',
@@ -12,6 +13,22 @@ export default function RootLayout({ children }) {
         <header className="flex flex-row justify-between">
           <Logo />
           {/* <button className="m-6 text-xl font-bold text-white">+</button> */}
+          <div className="mr-4 content-center">
+            <Image
+              alt="edit"
+              src="/edit.png"
+              width="20"
+              height="20"
+              className="m-2 inline"
+            />
+            <Image
+              alt="new"
+              src="/new.png"
+              width="20"
+              height="20"
+              className="m-2 inline"
+            />
+          </div>
         </header>
         <main>{children}</main>
       </body>
