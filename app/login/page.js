5f952a0e2cdf2b112ea6f5856f1c3d2b86d9e733
Daniel-Lidman <dlidman@gmail.com>
2025-07-14 14:15:34 -0500

    created login page bones. Next up, adding supabase auth

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Login | DL Video Inspiration Board',
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>L</h1>
+      <p>hello world!</p>
+    </div>
+  );
+}
